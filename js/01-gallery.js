057d04dc46a16cd51334bb620ac1363982cbed05
Arkadiy Byts <bytsarkadiy@gmail.com>
2023-04-01 15:14:53 +0200

    01.04

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,39 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+ console.log(galleryItems);
 
 const smallImg = galleryItems
-    .map((image) => `<a href='${image.original}'><img src='${image.preview}' 
+    .map((image) => `<div><a href='${image.original}'><img src='${image.preview}' 
     data-source='${image.original}'
-    alt='${image.description}'/></a>`)
+    alt='${image.description}'/></a></div>`)
     .join("  ");
 
 const galleryList = document.querySelector("div");
 console.log(galleryList);
 galleryList.insertAdjacentHTML('afterbegin', smallImg);
 
-const imgLinksContainers = document.querySelectorAll("div");
-for (const container of imgLinksContainers) {
-    container.classList
-    .add("gallery__item");
-};
-//below we remove class that was signed to main container earlier
-galleryList.classList.remove("gallery__item");
-
-const imgLinks = document.querySelectorAll("a");
-for (const link of imgLinks) {
-    link.classList
-    .add("gallery__link");
-};
-
-const images = document.querySelectorAll("img");
-for (const image of images) {
-    image.classList
-    .add("gallery__image");
-};
-
-
-
+const galeryItems = galleryList.children;
+console.log(galeryItems);
+for (const galeryItem of galeryItems) {
+    galeryItem.classList.add("gallery__item")
+}
 
 console.log();
+// const imgLinks = document.querySelectorAll("a");
+// for (const link of imgLinks) {
+//     link.classList
+//     .add("gallery__link");
+// };
+
+// const images = document.querySelectorAll("img");
+// for (const image of images) {
+//     image.classList
+//     .add("gallery__image");
+// };
+
+
+
+

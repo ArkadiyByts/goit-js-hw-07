9ec5e9aab0c280b38e0eb4d738ae24428f2e9e25
Arkadiy Byts <bytsarkadiy@gmail.com>
2023-04-03 11:35:21 +0200

    03.04 add eventlistener

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -15,9 +15,30 @@ galleryList.insertAdjacentHTML('afterbegin', smallImg);
 
 const galeryItems = galleryList.children;
 console.log(galeryItems);
+
+//here I have some problems with original image link
 for (const galeryItem of galeryItems) {
     galeryItem.classList.add("gallery__item")
+    galeryItem.addEventListener('click', (event) => {
+    event.preventDefault();
+    basicLightbox.create(`<img width="1400" height="900" src='https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg'>`).show();
+});
 }
+
+
+const galeryLinks = document.querySelectorAll("div > a");
+console.log(galeryLinks);
+for (const galeryLink of galeryLinks) {
+    galeryLink.classList.add("gallery__link");
+}
+
+const galeryImages = document.querySelectorAll('img');
+console.log(galeryImages);
+for (const galeryImg of galeryImages) {
+    galeryImg.classList.add("gallery__image");
+}
+
+
 
 console.log();
 // const imgLinks = document.querySelectorAll("a");
